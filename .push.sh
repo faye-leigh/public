ea7604c811cbed65822a31ec224f7afa1c65040c
L13 <faye@fayeleigh.com>
2024-10-25 09:05:26 -0400

    update from laptop

--- .push.sh
+++ .push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kitty sh -c 'cd /home/faye/git ; git add . ; git commit -a -m "update from laptop" ; git push ; read test'
+kitty sh -c 'echo pushing to faye-leigh/git ; cd /home/faye/git ; git add . ; git commit -a -m "update from laptop" ; git push ; read test'
 
 # kitty sh -c 'eval $(echo -e test) && read hold'
 # kitty sh -c 'echo test && read hold'
