882bddb06ec9673103b53da5c2874dabf43e30d5
L13 <faye@fayeleigh.com>
2024-10-25 09:04:23 -0400

    update from laptop

--- .push.sh
+++ .push.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-kitty --hold cd /home/faye/ ; git add . ; git commit -a -m "update from laptop" ; git push ; read test
+kitty sh -c 'cd /home/faye/git ; git add . ; git commit -a -m "update from laptop" ; git push ; read test'
 
+# kitty sh -c 'eval $(echo -e test) && read hold'
+# kitty sh -c 'echo test && read hold'
