7df215bc3d05260eb40bd5fe786994d9024ffbea
L13 <faye@fayeleigh.com>
2024-10-31 11:09:50 -0400

    update from laptop

--- push.sh
+++ push.sh
@@ -2,4 +2,9 @@
 
 # Push changes
 
-kitty sh -c 'echo "Pushing changes to faye-leigh/git:" ; cd ~/git ; git add . ; git commit -a -m "update from laptop" ; git push ; printf "\nFinished\n\nPress enter to exit:" ; read wait'
+kitty sh -c 'echo "Pushing changes to faye-leigh/git:" ; 
+    cd ~/git ; git add . ; 
+    git commit -a -m "update from laptop" ; 
+    git push ; 
+    echo -e "\nFinished\n\nPress enter to exit:" ; 
+    read wait'
