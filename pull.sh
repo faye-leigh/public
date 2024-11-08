ddc585184c24d03c7a6d74ab3547ec16819eb0a9
L13 <faye@fayeleigh.com>
2024-11-08 09:17:27 -0500

    update from laptop

--- pull.sh
+++ pull.sh
@@ -6,6 +6,22 @@ kitty sh -c '
     cd ~/git 
     git pull
     
+    echo -e "Pulling changes from faye-leigh/documents:\n" 
+    cd ~/documents 
+    git pull
+
+    echo -e "Pulling changes from faye-leigh/osu:\n" 
+    cd ~/osu 
+    git pull
+
+    echo -e "Pulling changes from faye-leigh/music:\n" 
+    cd ~/music 
+    git pull
+
+    echo -e "Pulling changes from faye-leigh/photos:\n" 
+    cd ~/photos 
+    git pull
+
     echo -e "\nFinished"
     read -p "Press enter to exit: " wait
 '
--- push.sh
+++ push.sh
@@ -8,6 +8,30 @@ kitty sh -c '
     git commit -a -m "update from laptop" 
     git push
     
+    echo -e "Pushing changes to faye-leigh/documents:\n" 
+    cd ~/documents
+    git add . 
+    git commit -a -m "update from laptop" 
+    git push
+
+    echo -e "Pushing changes to faye-leigh/photos:\n" 
+    cd ~/photos
+    git add . 
+    git commit -a -m "update from laptop" 
+    git push
+
+    echo -e "Pushing changes to faye-leigh/music:\n" 
+    cd ~/music
+    git add . 
+    git commit -a -m "update from laptop" 
+    git push
+
+    echo -e "Pushing changes to faye-leigh/osu:\n" 
+    cd ~/osu
+    git add . 
+    git commit -a -m "update from laptop" 
+    git push
+
     echo -e "\nFinished"
     read -p "Press enter to exit: " wait
 '
