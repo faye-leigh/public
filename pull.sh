0a9fffccb540b93dfe26c8c05b8ed50513585b8e
L13 <faye@fayeleigh.com>
2025-01-15 14:23:37 -0500

    update from laptop

--- pull.sh
+++ pull.sh
@@ -2,26 +2,8 @@
 
 # Pull changes
 kitty sh -c '
-    echo -e "Pulling changes from faye-leigh/git:\n" 
-    cd ~/git 
+    echo -e "Pulling changes:\n"  
     git pull
-    
-    echo -e "Pulling changes from faye-leigh/documents:\n" 
-    cd ~/documents 
-    git pull
-
-    echo -e "Pulling changes from faye-leigh/osu:\n" 
-    cd ~/osu 
-    git pull
-
-    echo -e "Pulling changes from faye-leigh/music:\n" 
-    cd ~/music 
-    git pull
-
-    echo -e "Pulling changes from faye-leigh/photos:\n" 
-    cd ~/photos 
-    git pull
-
     echo -e "\nFinished"
     read -p "Press enter to exit: " wait
 '
--- push.sh
+++ push.sh
@@ -2,36 +2,10 @@
 
 # Push changes
 kitty sh -c '
-    echo -e "Pushing changes to faye-leigh/git:\n" 
-    cd ~/git 
+    echo -e "Pushing changes:\n" 
     git add . 
     git commit -a -m "update from laptop" 
     git push
-    
-    echo -e "Pushing changes to faye-leigh/documents:\n" 
-    cd ~/documents
-    git add . 
-    git commit -a -m "update from laptop" 
-    git push
-
-    echo -e "Pushing changes to faye-leigh/photos:\n" 
-    cd ~/photos
-    git add . 
-    git commit -a -m "update from laptop" 
-    git push
-
-    echo -e "Pushing changes to faye-leigh/music:\n" 
-    cd ~/music
-    git add . 
-    git commit -a -m "update from laptop" 
-    git push
-
-    echo -e "Pushing changes to faye-leigh/osu:\n" 
-    cd ~/osu
-    git add . 
-    git commit -a -m "update from laptop" 
-    git push
-
     echo -e "\nFinished"
     read -p "Press enter to exit: " wait
 '
